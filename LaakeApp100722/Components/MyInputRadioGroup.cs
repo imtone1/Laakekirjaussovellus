2a32bded61191fb2e3567ad3f023efd56a50239e
imtone1 <88165529+imtone1@users.noreply.github.com>
2022-07-15 21:08:47 +0300

    Create, Details

---
+++ LaakeApp100722/Components/MyInputRadioGroup.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace LaakeApp100722.Components;
+
+public class MyInputRadioGroup<TValue> : InputRadioGroup<TValue>
+{
+   private string _name;
+   private string _fieldClass;
+
+   protected override void OnParametersSet()
+   {
+      //fielsindetifier of empty string if not fieldindentifier
+      var fieldClass = EditContext?.FieldCssClass(FieldIdentifier) ?? string.Empty;
+      if (fieldClass != _fieldClass || Name != _name)
+      {
+
+         _fieldClass = fieldClass;
+         _name = Name;
+         base.OnParametersSet();
+      }
+   }
+}
