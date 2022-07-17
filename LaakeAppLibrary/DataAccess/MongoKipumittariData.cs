d7df35642b751ddfdcab58b78002b92203dcd707
imtone1 <88165529+imtone1@users.noreply.github.com>
2022-07-17 20:17:06 +0300

    css muotoilut

---
+++ LaakeAppLibrary/DataAccess/MongoKipumittariData.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+
+namespace LaakeAppLibrary.DataAccess;
+public class MongoKipumittariData
+{
+   private readonly IMongoCollection<KipumittariModel> _kipumittari;
+   private readonly IMemoryCache _cache;
+}
