9fe2dfb5a056ebd06aa8082b5ff9628feed0e6e3
imtone1 <88165529+imtone1@users.noreply.github.com>
2022-07-18 15:35:12 +0300

    mongomodel, sample page alku

---
+++ LaakeAppLibrary/DataAccess/ILaakeData.cs
@@ -0,0 +1,9 @@
+﻿namespace LaakeAppLibrary.DataAccess;
+
+public interface ILaakeData
+{
+   Task CreateLaake(LaakeModel laake);
+   Task<LaakeModel> GetLaake(string id);
+   Task<List<LaakeModel>> GetUsersLaakkeet(string userId);
+   Task UpdateLaake(LaakeModel laake);
+}
