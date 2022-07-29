89fe4cb90d53c580f66be064a81f0e6a8bac1b11
imtone1 <88165529+imtone1@users.noreply.github.com>
2022-07-29 19:50:17 +0300

    Lisää oire toiminnallisuus

---
+++ LaakeApp100722/Models/CreateOireModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LaakeApp100722.Models;
+
+public class CreateOireModel
+{
+   [Required]
+   [MinLength(1)]
+   [Display(Name = "Oire")]
+
+   public string OireNimi { get; set; }
+
+
+   [Display(Name = "Oireen kuvaus")]
+   public string OireKuvaus { get; set; }
+
+   [Display(Name = "Pvm, aika")]
+   public DateTime OirePvm { get; set; } = DateTime.Now;
+}
